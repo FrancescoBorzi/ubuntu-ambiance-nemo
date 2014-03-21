133275ca435e83b92940c6d96805841e3f8bd7f2
ShinDarth <borzifrancesco@gmail.com>
2014-03-21 13:47:46 +0100

    Added support for Ubuntu 12.04

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
 sudo cp nemo.css /usr/share/themes/Ambiance/gtk-3.0/apps/
-echo "@import url(\"apps/nemo.css\");" | sudo tee -a /usr/share/themes/Ambiance/gtk-3.0/gtk-main.css
+
+if [ -f /usr/share/themes/Ambiance/gtk-3.0/gtk-main.css ];
+then
+	echo "@import url(\"apps/nemo.css\");" | sudo tee -a /usr/share/themes/Ambiance/gtk-3.0/gtk-main.css
+else
+	if [ -f /usr/share/themes/Ambiance/gtk-3.0/gtk.css ];
+	then
+		echo "@import url(\"apps/nemo.css\");" | sudo tee -a /usr/share/themes/Ambiance/gtk-3.0/gtk.css
+	fi
+fi
